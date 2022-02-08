0e0f3ad12b8a0b2d07428ea8b49fa776050c838d
Singithi Kandage <skandage@nuvelance.io>
2022-02-08 15:04:19 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Address Book</h1>
+      <p>Edit me!</p>
+    </div>
+  );
+}
+
+export default App;
